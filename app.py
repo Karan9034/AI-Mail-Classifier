f3b5b2c8c2647f01dd2f15963385208c9dc84d77
Karan9034 <karan.agr9034@gmail.com>
2020-12-03 15:42:17 +0530

    Forms Completed

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template, url_for, flash,redirect
+import os
+import secrets
+from flask import Flask, request, render_template, url_for, flash, redirect, current_app
 from form import TestForm, TrainForm
+from werkzeug.utils import secure_filename
+
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '18256fdc199f95f0cdac2b6ddbae9214'
 
+
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    testForm = TestForm()
+    trainForm = TrainForm()
+    if testForm.validate_on_submit():
+        f = testForm.testData.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.instance_path, 'static/data', filename))
+        return redirect(url_for('home'))
+    if trainForm.validate_on_submit():
+        pass
+    return render_template('index.html', testForm=testForm, trainForm=trainForm)
 
 
 @app.route('/home')
--- form.py
+++ form.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class TestForm(FlaskForm):
-    testData = 
+    testData = FileField('Testing Data:', validators=[FileRequired(), FileAllowed(['zip'])])
+    submit = SubmitField('Test Your Model')
 
 class TrainForm(FlaskForm):
-    trainData = 
+    trainData = FileField('Training Data:', validators=[FileRequired(), FileAllowed(['zip'])])
+    learnRate = SelectField('Learn Rate:', choices=[('','--select--'),('','1e-5'),('','1e-4'),('','0.5e-5'),('', 'Custom')])
+    epochs = SelectField('Epochs:', choices=[('','--select--'),('','10'),('','5'),('','15'),('', 'Custom')])
+    arch = SelectField('Model Archiecture:', choices=[('','--select--'),('','XLNet (Recommended)'),('','T5'),('','BERT'),('', 'GPT2')])
+    submit = SubmitField('Retrain Your Model')
